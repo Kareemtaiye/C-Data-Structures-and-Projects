095df650c64fb863bfe22721639183f22957b087
Kareem Taiye <olayinkahassan117@gmail.com>
2025-06-24 20:29:39 +0100

    well understood knowlegde of data types in C, and their system level size in the machine's memory

---
+++ 00-c-basics/hello-world.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+int main()
+{
+    printf("Hello world");
+
+    int two = 2, three = 3, six = 6;
+    int result = (two + three) * (six / three);
+
+    printf("%d", result);
+
+    return 0;
+}
