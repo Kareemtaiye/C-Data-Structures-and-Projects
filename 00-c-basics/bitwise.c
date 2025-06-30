26d4f61c1a0d425c3c0c437a2068fa307c4b5fea
Kareem Taiye <olayinkahassan117@gmail.com>
2025-06-30 01:13:34 +0100

    bitwise operators

---
+++ 00-c-basics/bitwise.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() 
+{
+    int a = 10;//(00001010)
+    int b = 3; //(00000011)
+    //Bitwise and(&)
+    printf("%d\n", a & b);
+
+    //Bitwise or(|)
+    printf("%d\n", a | b); 
+
+    //Bitwise left shift (for a << b, result = a * 2^b)
+    printf("%d\n", a << b); //(10100000)
+
+    //Bitwise right shift (for a << b, result = a / 2^b)
+    printf("%d\n", a >> b); //(00000001)
+
+    return 0;
+}
