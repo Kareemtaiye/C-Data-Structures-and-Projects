cbb5141dd014bde47cf523608f1471310c05d120
Kareem Taiye <olayinkahassan117@gmail.com>
2025-07-02 22:15:11 +0100

    count line fucntion done

--- 01-c-beyond-basics/char-count.c
+++ 01-c-beyond-basics/char-count.c
@@ -1,14 +1,30 @@
 #include <stdio.h>
 
+/* Count character v-1 */
+long countInputV1() {
+    long nc = 0;
+    int c;
 
- /* Count character v-1 */
-int main()
-{
-    int inc;
-
-    while(getchar() != EOF) 
-    {
-        ++inc;
-        printf("%d\n", inc);
+    while ((c = getchar()) != '\n' && c != EOF) {
+        ++nc;
     }
+
+    return nc;
+}
+
+/* Count characters v-2*/
+long countInputV2() {
+    long nc = 0;
+    int c;
+    for (nc = 0; (c = getchar()) != '\n' && c != EOF; nc++);
+
+    return nc;
+}
+
+int main() {
+    printf("Type your characters:\n");
+    long characters = countInputV2();
+    printf("Characters typed %ld\n", characters);
+
+    return 0;
 }
---
+++ 01-c-beyond-basics/count-line.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+int countLines() {}
+
+int main() { return 0; }
