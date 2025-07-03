4495fa42ce418695b2108991c9f1982e2c444795
Kareem Taiye <olayinkahassan117@gmail.com>
2025-07-03 21:17:42 +0100

    added armstrong v-2

--- 02-c-problem-solving/armstrong.c
+++ 02-c-problem-solving/armstrong.c
@@ -12,10 +12,12 @@ int checkArmstrong(int digit) {
         count++;
     }
 
+    /* Convert the number to number */
     char str[20];
     sprintf(str, "%d", original);
     int result = 0;
 
+    /* Convert each digits back to string and perform logic */
     for (int i = 0; i < count; i++) {
         int base_val = str[i] - '0';
         int exponent = count;
@@ -24,6 +26,32 @@ int checkArmstrong(int digit) {
     }
 
     return result == original;
+}
+
+/* Implementation v-2 */
+int checkArmstrongV2(int digit) {
+    int count = 0;
+    int original = digit;
+
+    while (digit != 0) {
+        digit = digit / 10;
+        ++count;
+    }
+
+    int result = 0;
+    int digit_copy = original;
+
+    /* Extract the last values from the right until there is no more value*/
+    int rem = digit_copy % 10;
+    while (digit_copy != 0) {
+        int base_value = rem;
+        int exponent = count;
+        result = result + (int)pow(base_value, exponent);
+        digit_copy = digit_copy / 10;
+        rem = digit_copy % 10;
+    }
+
+    return original == result;
 }
 
 int main() {
@@ -38,5 +66,7 @@ int main() {
     } else {
         printf("value: %d is not an armstrong number", value);
     }
+
+    // printf("%d", checkArmstrongV2(153));
     return 0;
 }
